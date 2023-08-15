40294906948b746a6913bbcc384f30eeb8042517
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-15 19:55:52 +0200

    added open/close buttons for mobile menu; depending on the size of the user's screen we offer different menus

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
     <div
       className="flex justify-between items-center 
@@ -13,23 +18,30 @@ const Navbar = () => {
       >
         REACT.
       </h1>
-      <ul className="flex hidden">
+      <ul className="hidden md:flex">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
-      <div>
-        <AiOutlineMenu size={20} />
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <div className="fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]">
+      <div
+        className={
+          !nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "fixed left-[-100%]"
+        }
+      >
         <h1
           className="w-full text-3xl 
       font-bold text-green-400 m-4"
         >
           REACT.
         </h1>
+
         <ul className="uppercase p-4">
           <li className="p-4 border-b border-gray-600">Home</li>
           <li className="p-4 border-b border-gray-600">Company</li>
