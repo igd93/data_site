083e28b0a4c5136651af1a0b603b97e384cae2e3
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-15 19:42:14 +0200

    added mobile menu; not working yet

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   return (
@@ -12,13 +13,31 @@ const Navbar = () => {
       >
         REACT.
       </h1>
-      <ul className="flex">
+      <ul className="flex hidden">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
+      <div>
+        <AiOutlineMenu size={20} />
+      </div>
+      <div className="fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]">
+        <h1
+          className="w-full text-3xl 
+      font-bold text-green-400 m-4"
+        >
+          REACT.
+        </h1>
+        <ul className="uppercase p-4">
+          <li className="p-4 border-b border-gray-600">Home</li>
+          <li className="p-4 border-b border-gray-600">Company</li>
+          <li className="p-4 border-b border-gray-600">Resources</li>
+          <li className="p-4 border-b border-gray-600">About</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </div>
     </div>
   );
 };
