ff747c1e5459c9983265a7f3cc8123c09e787222
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-16 22:06:52 +0200

    finished and styled analytics component

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import { data } from '../assets/data';
+import React from "react";
+import { data } from "../assets/data";
 
 const Analytics = () => {
   return (
-    <div className='width-full bg-white px-4'>
-      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-        <img src={data[0]} alt="Laptop" />        
+    <div className=" min-h-max w-full bg-white py-16 px-4">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+        <img className="w-[500px] mx-auto my-4 rounded" src={data[0].image} alt="Laptop" />
+        <div className="flex flex-col justify-center">
+          <p className="text-green-400 font-bold text-xl">DATA ANALYTICS DASHBOARD</p>
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">Manage Data Analytics Centrally</h1>
+          <p className="text-xl">
+            Unlock the true potential of your organization's data landscape with
+            our cutting-edge Data Analytics Centralization Services. Revel in
+            the power of comprehensive insights as our services reveal hidden
+            correlations, enabling innovation and growth. Seamlessly balancing
+            control and flexibility, we tailor analytics to your unique needs,
+            transforming raw data into a strategic asset. Join us in
+            revolutionizing data analytics, where human-like expertise meets
+            technological prowess, and steer your enterprise towards a future
+            defined by data-driven excellence.
+          </p>
+          <button className="bg-black w-[200px] rounded-md font-medium 
+        my-6 py-3 mx-auto md:mx-0 text-green-400">Get started</button>
+        </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Analytics
+export default Analytics;
