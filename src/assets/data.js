afdf2432f851565cc4762837d78cc39398784060
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-17 20:26:19 +0200

    added additional icons

--- src/assets/data.js
+++ src/assets/data.js
@@ -4,7 +4,7 @@ export const data = [
     image:
       "https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80",
   },
-  { id: 2, image: "" },
-  { id: 3, image: "" },
-  { id: 4, image: "" },
+  { id: 2, image: "https://freesvg.org/img/1367934593.png" },
+  { id: 3, image: "https://freesvg.org/img/People_044_Talking_Bubble.png" },
+  { id: 4, image: "https://freesvg.org/img/Originuum---Vetor---Plano---Grupo-de-Profissionais---1.0.0.png" },
 ];
