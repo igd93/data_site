a4dda01bc9df809f7f9d732a5ad90f97caefb0ca
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-19 20:13:22 +0200

    small fixes of a mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
         <li className="p-4">Contact</li>
       </ul>
       <div onClick={handleNav} className="block md:hidden">
-        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
       <ul
         className={
